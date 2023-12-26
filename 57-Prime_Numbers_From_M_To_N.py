5611b7985cab5b36892f38ac50400f2623eaa4e5
Divyanshu Kumar <113873506+DudeDiv@users.noreply.github.com>
2023-12-26 21:08:48 +0530

    Update 57-Prime_Numbers_From_M_To_N.py

--- 57-Prime_Numbers_From_M_To_N.py
+++ 57-Prime_Numbers_From_M_To_N.py
@@ -1,2 +1,21 @@
 #A function is given in the prefilled code that takes two numbers M and N as arguments.
 #Write a program that returns all the prime numbers from M to N separated by a space.
+def check_is_prime(m, n):
+    prime_numbers_list = []
+    for i in range(m,n+1):
+        flag = False
+        if(i==0 or i==1):
+            flag = True
+        for j in range(2,i):
+            if(i%j==0):
+                flag = True
+                break
+        if(flag == False):
+            prime_numbers_list.append(str(i))
+    prime_numbers_list = " ".join(prime_numbers_list)
+    return prime_numbers_list
+    
+m = int(input())
+n = int(input())
+prime_numbers  = check_is_prime(m,n)
+print(prime_numbers)
