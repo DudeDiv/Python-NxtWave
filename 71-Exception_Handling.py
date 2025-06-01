48f5fc6c876ef927427689563d407297c9052a39
Divyanshu Kumar <113873506+DudeDiv@users.noreply.github.com>
2025-06-02 00:18:11 +0530

    Create 71-Exception_Handling.py

---
+++ 71-Exception_Handling.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+T = int(input())
+for i in range(T):
+    try:
+        a,b = input().split()
+        print(int(a)//int(b))
+        
+    except ZeroDivisionError as e:
+        print("Error Code:",e)
+        
+    except ValueError as e:
+        print("Error Code:",e)
